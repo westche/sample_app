d208a84f60cc7a00ed861928325cbc1bac30675e
Art West <Art.westiv@gmail.com>
2019-01-27 16:13:44 -0500

    added tweets

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 
 module SampleApp
   class Application < Rails::Application
@@ -16,6 +18,6 @@ module SampleApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    #config.app_generators.scaffold_controller = :scaffold_controller
+    config.app_generators.scaffold_controller = :scaffold_controller
   end
 end
