b1c1687172fe67781e47cbd7ba4e4332b122a10b
Art West <Art.westiv@gmail.com>
2019-01-28 20:26:07 -0500

    validations and styling

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,3 +1,11 @@
 class Run < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence:true
+  validates :Run_Type, presence:true
+  validates :Location, presence:true
+  validates :Start_Time, presence:true
+  validates :Pace, presence:true, length: {maximum: 5}
+  validates :Miles, presence:true, length: {maximum: 5}
+  validates :Run_Date, presence:true
 end
